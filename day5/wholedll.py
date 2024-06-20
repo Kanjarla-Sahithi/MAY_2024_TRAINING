29e06f1dd959c54c53a84876af55ddd1af85a2cd
Kanjarla Sahithi <108618620+Kanjarla-Sahithi@users.noreply.github.com>
2024-06-20 21:36:27 +0530

    Create wholedll.py

---
+++ day5/wholedll.py
@@ -0,0 +1,160 @@
+#We canot perform binary search in linked list because there is no indexing for linked list
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.prev=None
+        self.next=None
+
+class dll:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+
+    def addback(self,data):
+        if self.head==None:
+            self.head=node(data)
+            self.tail=self.head
+        else:
+            self.tail.next=node(data)
+            self.tail.next.prev=self.tail
+            self.tail=self.tail.next
+
+    def addfront(self,data):
+        if self.head==None:
+            self.head=node(data)
+            self.tail=self.head
+        else:
+            newnode=node(data)
+            self.head.prev=node(data)
+            newnode.next=self.head
+            self.head=newnode
+
+    def display(self):
+        t=self.head
+        while t!=None:
+            print(t.data,end="<=>")
+            t=t.next
+
+    def reverse_display(self):
+        t=self.tail
+        while(t!=None):
+            print(t.data, end="<-->")
+            t=t.prev
+
+    def linearsearch(self,key):
+        if self.head==None:
+            return "List is empty"
+        else:
+            h=self.head
+            t=self.tail
+            while t!=h and t.next!=h :
+                if h.data==key:
+                    return "found"
+                elif t.data==key:
+                    return "found"
+                else:
+                    h=h.next
+                    t=t.prev
+                if h.data==t.data:
+                    return "found"
+            return "notfound"
+
+    def evenoddlen(self):
+        if self.head==None:
+            return "List is empty"
+        else:
+            h=self.head
+            t=self.tail
+            c=0
+            while t!=h and t.next!=h :
+                c+=2
+                t=t.prev
+                h=h.next
+            if t==h:
+                c+=1
+            if c%2==0:
+                return "Even"
+            else:
+                return "Odd"
+
+    def ispalindrome(self):
+        if self.head==None:
+            return "palindrome"
+        else:
+            h=self.head
+            t=self.tail
+            while t!=h and t.next!=h :
+                if h.data!=t.data:
+                    return "not palindrome"
+                h=h.next
+                t=t.prev
+        return "palindrome"
+
+    def splitting(self):
+        if self.head==None:
+            return "Empty list"
+        else:
+            slow=self.head
+            fast=self.head
+            while fast and fast.next:
+                slow=slow.next
+                fast=fast.next.next
+            tp=self.head
+            t=slow.prev
+            self.tail.next=tp
+            tp.prev=self.tail
+            slow.prev.next=None
+            slow.prev=None
+            self.head=slow
+            self.tail=t
+
+    def swap(self):
+        t1=self.head
+        t2=t1.next
+        t3=t2.next
+        while t2:
+            if t1==self.head:
+                t1.next=t3
+                t3.next=t1
+                t2.next=t1
+                t1.prev=t2
+                t2.prev=None
+                self.head=t2
+                obj2.display()
+            else:
+                pass
+            
+l=[1,2,3,4,5]
+l1=[5,7,8,2,1,4]
+#obj=dll()
+#obj1=dll()
+obj2=dll()
+'''for i in l:
+    obj.addfront(i)
+obj.display()
+print()
+for i in l:
+    obj1.addback(i)
+print()'''
+for i in l1:
+    obj2.addback(i)
+obj2.display()
+print()
+#obj2.reverse_display()
+print()
+#print(obj2.linearsearch(7))
+#print(obj2.evenoddlen())
+#print(obj2.ispalindrome())
+#obj2.splitting()
+#obj2.display()
+#print()
+#obj2.reverse_display()
+
+obj2.display()
+obj2.swap()
+obj2.display()
+
+
+
+
+
