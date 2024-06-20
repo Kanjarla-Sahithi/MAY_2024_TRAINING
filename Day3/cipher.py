bf407702c6e078afe64d3b8172573d9f49256f59
Kanjarla Sahithi <108618620+Kanjarla-Sahithi@users.noreply.github.com>
2024-06-20 21:34:00 +0530

    Create cipher.py

---
+++ Day3/cipher.py
@@ -0,0 +1,15 @@
+"""ip:  khoor
+      key-3"""
+string=input()
+key=3
+"""decrypt=[]
+for s in string:
+        decrypt.append(chr((ord(s)-key-97)%26+97))
+n="".join(decrypt)
+print(n)"""
+
+
+
+
+      
+
